24aa909a90153720903c35ba7d1157c4e8e2fd8b
kutayozel <zelkutay@gmail.com>
2022-10-05 20:08:02 +0300

    react hot toast animation on add

--- components/Product.tsx
+++ components/Product.tsx
@@ -2,13 +2,24 @@ import { ShoppingCartIcon } from "@heroicons/react/outline";
 import Image from "next/image";
 import React from "react";
 import { urlFor } from "../sanity";
+import { useDispatch } from "react-redux";
+import { addToBasket } from "../redux/basketSlice";
+import toast from "react-hot-toast";
 
 interface ProductProps {
   product: Product;
 }
 
 const Product = ({ product }: ProductProps) => {
-  const addItemToBasket = () => {};
+  const dispatch = useDispatch();
+
+  const addItemToBasket = () => {
+    dispatch(addToBasket(product));
+
+    toast.success(`${product.title} added to basket`, {
+      position: "bottom-center",
+    });
+  };
 
   return (
     <div className="flex h-fit w-[320px] select-none flex-col space-y-3 rounded-xl bg-[#35383C] p-8 md:h-[500px] md:w-[400px] md:p-10">
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return;
-  <Provider store={store}>
-    <Component {...pageProps} />;
-  </Provider>;
+  return (
+    <Provider store={store}>
+      <Toaster />
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
