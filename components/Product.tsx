dffe13bad2391e67ff83b3babb9402406c967182
kutayozel <zelkutay@gmail.com>
2022-10-05 19:51:02 +0300

    redux functs

--- components/Product.tsx
+++ components/Product.tsx
@@ -8,6 +8,8 @@ interface ProductProps {
 }
 
 const Product = ({ product }: ProductProps) => {
+  const addItemToBasket = () => {};
+
   return (
     <div className="flex h-fit w-[320px] select-none flex-col space-y-3 rounded-xl bg-[#35383C] p-8 md:h-[500px] md:w-[400px] md:p-10">
       <div className="relative h-64 w-full md:h-72">
@@ -27,6 +29,7 @@ const Product = ({ product }: ProductProps) => {
         <div
           className="md:w[70px] flex h-16 w-16 flex-shrink-0 cursor-pointer items-center 
         justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 md:h-[70px] "
+          onClick={addItemToBasket}
         >
           <ShoppingCartIcon className="h-8 w-8 text-white" />
         </div>
